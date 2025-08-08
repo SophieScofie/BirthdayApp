620d72107c33e1327161277f23f4fb83c2f9fe89
Sophie <yummylemon8@gmail.com>
2025-08-08 12:48:07 -0700

    Initial Commit

---
+++ BirthdayApp/BirthdayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayAppApp.swift
+//  BirthdayApp
+//
+//  Created by Scholar on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
